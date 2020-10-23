7eac79ee2d7986e002daed46088883d32faecc47
Ricardo Blanco <rbl186@alumnos.unican.es>
2020-10-23 16:15:12 +0200

    Añadido el test de android para la vista detalle de la gasolinera

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/ExampleInstrumentedTest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/ExampleInstrumentedTest.java
@@ -2,13 +2,29 @@ package com.isunican.proyectobase;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 /**
  * Instrumented test, which will execute on an Android device.
  *

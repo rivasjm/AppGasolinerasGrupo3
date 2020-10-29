fd959bb4f631d85be60047725e64a06f05644075
Juan David Corrales Gil <jcg700@alumnos.unican.es>
2020-10-29 13:10:05 +0100

    Corrección y comprobación del buen funcionamiento de las pruebas de interfaz de usuario con Espresso.

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/BuscarGasolinerasPorUnTipoDeCombustibleUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/BuscarGasolinerasPorUnTipoDeCombustibleUITest.java
@@ -1,19 +1,14 @@
 package com.isunican.proyectobase;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import androidx.test.espresso.ViewAssertion;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.isunican.proyectobase.Model.Gasolinera;
 import com.isunican.proyectobase.Views.MainActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,15 +17,10 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.CoreMatchers.allOf;
+
 
 @RunWith(AndroidJUnit4.class)
 public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
@@ -39,18 +29,25 @@ public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void gasoleoA_Test() {
+    String[] arrayOperaciones;
 
+    @Before
+    public void preparacionTest() {
 
-
+        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        //
+        // Se pulsa el boton para acceder a los Filtros.
         onView(withId(R.id.buttonFiltros)).perform(click());
 
-        String[] arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
+        // Se guarda en un array los tipos de combustible que existen.
+        arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
+    }
 
+    @Test
+    public void tiposCombustible_Test() {
+
+        // Se comprueba seleccionando todos los tipos existentes y se comprueba su correspondiente nombre
         onView(withText(arrayOperaciones[0])).perform(click());
         onView(withText(arrayOperaciones[0])).check(matches(withText("Gasóleo A")));
 
@@ -66,48 +63,46 @@ public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
         onView(withText(arrayOperaciones[4])).perform(click());
         onView(withText(arrayOperaciones[4])).check(matches(withText("Gasóleo Premium")));
 
+        // Se deseleccionan todos los tipos de combustible existentes
         onView(withText(arrayOperaciones[4])).perform(click());
         onView(withText(arrayOperaciones[3])).perform(click());
         onView(withText(arrayOperaciones[2])).perform(click());
         onView(withText(arrayOperaciones[1])).perform(click());
-
-
-        //onView(withText("Aceptar")).check(matches(withText("Aceptar")));
-        //onView(withText("Cancelar")).check(matches(withText("Cancelar")));
-
-        onView(withText("Aceptar")).perform(click());
-
-        onData(withId(R.id.textViewTipoGasolina))
-                .inAdapterView(withId(R.id.listViewGasolineras))
-                .atPosition(0)
-                .check(matches(withText("Gasóleo A")));
-
-        /*
-        onData(anything()).inAdapterView(withId(R.id.listViewGasolineras)).atPosition(0).perform(click());
-        ListView ltmp = mActivityTestRule.getActivity().findViewById(R.id.listViewGasolineras);
-        */
+        onView(withText(arrayOperaciones[0])).perform(click());
 
     }
-    /*
+
+    @Test
+    public void gasoleoA_Test() {
+
+        // Se selecciona el segundo tipo de combustible que se corresponde con Gasolina 95
+        onView(withText(arrayOperaciones[0])).perform(click());
+
+        // Se pulsa el botón Aceptar dentro del menú desplegable
+        onView(withText("Aceptar")).perform(click());
+
+        // Se comprueba que el tipo de combustible se ha cambiado correctamente accediendo
+        // a la primera gasolinera y comprobando su textViewTipoGasolina
+        onData(anything())
+                .inAdapterView(withId(R.id.listViewGasolineras))
+                .atPosition(0)
+                .onChildView(withId(R.id.textViewTipoGasolina))
+                .check(matches(withText("Gasóleo A")));
+
+    }
+
     @Test
     public void gasolina95_Test() {
 
-        //
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        //
-        onView(withId(R.id.buttonFiltros)).perform(click());
-        //
-        String[] arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
-
+        // Se selecciona el segundo tipo de combustible que se corresponde con Gasolina 95
         onView(withText(arrayOperaciones[1])).perform(click());
-        onView(withText(arrayOperaciones[1])).check(matches(withText("Gasolina 95")));
 
         onView(withText("Aceptar")).perform(click());
 
-        onData(withId(R.id.textViewTipoGasolina))
+        onData(anything())
                 .inAdapterView(withId(R.id.listViewGasolineras))
                 .atPosition(0)
+                .onChildView(withId(R.id.textViewTipoGasolina))
                 .check(matches(withText("Gasolina 95")));
 
     }
@@ -115,22 +110,15 @@ public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
     @Test
     public void gasolina98_Test() {
 
-        //
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        //
-        onView(withId(R.id.buttonFiltros)).perform(click());
-        //
-        String[] arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
-
+        // Se selecciona el segundo tipo de combustible que se corresponde con Gasolina 98
         onView(withText(arrayOperaciones[2])).perform(click());
-        onView(withText(arrayOperaciones[2])).check(matches(withText("Gasolina 98")));
 
         onView(withText("Aceptar")).perform(click());
 
-        onData(withId(R.id.textViewTipoGasolina))
+        onData(anything())
                 .inAdapterView(withId(R.id.listViewGasolineras))
                 .atPosition(0)
+                .onChildView(withId(R.id.textViewTipoGasolina))
                 .check(matches(withText("Gasolina 98")));
 
     }
@@ -138,22 +126,15 @@ public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
     @Test
     public void biodiésel_Test() {
 
-        //
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        //
-        onView(withId(R.id.buttonFiltros)).perform(click());
-        //
-        String[] arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
-
+        // Se selecciona el segundo tipo de combustible que se corresponde con Biodiésel
         onView(withText(arrayOperaciones[3])).perform(click());
-        onView(withText(arrayOperaciones[3])).check(matches(withText("Biodiésel")));
 
         onView(withText("Aceptar")).perform(click());
 
-        onData(withId(R.id.textViewTipoGasolina))
+        onData(anything())
                 .inAdapterView(withId(R.id.listViewGasolineras))
                 .atPosition(0)
+                .onChildView(withId(R.id.textViewTipoGasolina))
                 .check(matches(withText("Biodiésel")));
 
     }
@@ -161,24 +142,17 @@ public class BuscarGasolinerasPorUnTipoDeCombustibleUITest {
     @Test
     public void gasóleoPremium_Test() {
 
-        //
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        //
-        onView(withId(R.id.buttonFiltros)).perform(click());
-        //
-        String[] arrayOperaciones = appContext.getResources().getStringArray(R.array.operacionesArray);
-
+        // Se selecciona el segundo tipo de combustible que se corresponde con Gasóleo Premium
         onView(withText(arrayOperaciones[4])).perform(click());
-        onView(withText(arrayOperaciones[4])).check(matches(withText("Gasóleo Premium")));
 
         onView(withText("Aceptar")).perform(click());
 
-        onData(withId(R.id.textViewTipoGasolina))
+        onData(anything())
                 .inAdapterView(withId(R.id.listViewGasolineras))
                 .atPosition(0)
+                .onChildView(withId(R.id.textViewTipoGasolina))
                 .check(matches(withText("Gasóleo Premium")));
 
     }
-    */
+
 }

30cc7c354a2737d94a96eb3f7124eb76750329c2
mizu-git <hamza.hamda@alumnos.unican.es>
2020-11-30 21:04:53 +0100

    Intentando resolver el error de travis
    Es posible que el emulador se abra con la pantalla bloqueda, se ha añadiodo un setup a la prueba de interfaz para desbloquear el terminal antes de ejecutar la prueba de interfaz.

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/PruebaUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/PruebaUITest.java
@@ -1,15 +1,23 @@
 package com.isunican.proyectobase.Views;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.view.WindowManager;
+
+import androidx.test.annotation.UiThreadTest;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.isunican.proyectobase.R;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.content.Context.KEYGUARD_SERVICE;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 
@@ -18,7 +26,26 @@ public class PruebaUITest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    @UiThreadTest
+    @Before
+    public void setUp() throws Throwable {
+        final Activity activity = mActivityTestRule.getActivity();
+        mActivityTestRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                KeyguardManager mKG = (KeyguardManager) activity.getSystemService(Context.KEYGUARD_SERVICE);
+                KeyguardManager.KeyguardLock mLock = mKG.newKeyguardLock(KEYGUARD_SERVICE);
+                mLock.disableKeyguard();
 
+                //turn the screen on
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+            }
+        });
+    }
     @Test
     public void tiposCombustible_Test() {
         onView(ViewMatchers.withId(R.id.info)).perform(click());
